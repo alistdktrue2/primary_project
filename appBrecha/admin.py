3dcd21f27c6ed6e41b705ff91bc037b3d8e6615d
alistdktrue2 <alistdktrue2@gmail.com>
2023-10-18 00:28:41 -0500

    requeriments dc9

--- appBrecha/admin.py
+++ appBrecha/admin.py
@@ -2,12 +2,12 @@
 from django.contrib import admin
 from .models import Contact
 from django import forms
-from phonenumber_field.widgets import PhoneNumberPrefixWidget
+
 
 class ContactForm(forms.ModelForm):
     class Meta:
-        widgets = {                          # Here
-            'phone': PhoneNumberPrefixWidget(initial='US'),
+        widgets = {                       
+            'phone': "phone",
         }
 
 @admin.register(Contact)
