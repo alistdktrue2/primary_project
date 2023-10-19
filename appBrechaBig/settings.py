3059c9f2bcafb58c22e83acf18fab1b8cd5906a1
alistdktrue2 <alistdktrue2@gmail.com>
2023-10-18 20:12:55 -0500

    requeriments dd9

--- appBrechaBig/settings.py
+++ appBrechaBig/settings.py
@@ -122,6 +122,12 @@ MEDIA_ROOT = BASE_DIR / 'images'
 
 
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
